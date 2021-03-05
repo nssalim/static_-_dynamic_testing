e83a81b8f10f1c94260897c97b644a82ee230625
Nargis <nargis.salim@btinternet.com>
2021-03-05 05:22:10 +0000

    testing

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,25 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.game = CardGame("Game")
+
+        self.card1 = Card("hearts", 1)
+        self.card2 = Card("diamonds", 2)
+        self.card3 = Card("clubs", 4)
+        self.card4 = Card("spades", 3)
+
+        self.hand = [self.card1, self.card2, self.card3, self.card4]
+
+    def test_check_card_is_ace(self):
+        self.assertEqual(True, self.game.card_is_ace(self.card1))
+
+    def test_check_card_not_ace(self):
+        self.assertEqual(False, self.game.card_is_ace(self.card2))
+
+    def test_highest_card(self):
+        self.assertEqual(self.card3, self.game.highest_card(self.card3, self.card4))
+
+    def test_cards_total(self):
+        self.assertEqual("Your winning total is 10", self.game.cards_total(self.hand))
--- part_2_code/src/card_game.py
+++ part_2_code/src/card_game.py
@@ -1,29 +1,25 @@
 ### Testing task 2 code:
-
 # Carry out dynamic testing on the code below.
-
 # Correct the errors below that you spotted in task 1.
 
 class CardGame:
+  def __init__(self, name):
+    self.name = name
 
-
-  def check_for_ace(self, card):
-    if card.value = 1:
+  def card_is_ace(self, card):
+    if card.value == 1:
       return True
-    else
+    else:
       return False
-   
 
-  dif highest_card(self, card1 card2):
-  if card1.value > card2.value:
-    return card
-  else:
-    return card2
-  
+  def highest_card(self, card1, card2):
+    if card1.value > card2.value:
+      return card1
+    else:
+      return card2
 
-
-def cards_total(self, cards):
-  total
-  for card in cards:
-    total += card.value
-    return "You have a total of" + total
+  def cards_total(self, cards):
+    total = 0
+    for card in cards:
+      total += card.value
+    return "Your winning total is " + str(total)
